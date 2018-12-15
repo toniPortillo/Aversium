a1f785fdab75af8a215c6e0c347c6475dc15ca3c
Toni <toniplsec@gmail.com>
2018-12-15 19:50:55 +0100

    En proceso de implementacion de la modificacion de equipos

--- router/controllers/teamController.js
+++ router/controllers/teamController.js
@@ -1,6 +1,6 @@
 'use strict';
-let fetch = require('node-fetch');
 let Team = require('../../models/mongoModels/teamModel').Team;
+let User = require('../../models/mongoModels/userModel').User;
 
 exports.team_list_get = (req, res) => {
     let list = [];
@@ -70,9 +70,13 @@ exports.team_delete_post = (req, res) => {
 
 exports.team_modify_get = (req, res) => {
     
+    let err = "";
     Team.find({teamname: req.params.nombre})
     .then((team) => {
-        res.render('modifyTeam.ejs', {team: team});
+        res.render('modifyTeam.ejs', {
+            team: team,
+            err: err
+        });
     })
     .catch((err) => {
         if(err) throw err;
@@ -82,7 +86,66 @@ exports.team_modify_get = (req, res) => {
 
 exports.team_modify_post = (req, res) => {
 
-    res.send(req.body);
+    
+    //User.find({username: req.body.users})
+    //.then((user) => {
+        /*Team.findOneAndUpdate({teamname: req.params.nombre}, 
+            {maxmembers:req.body.maxmembers, users: user}, {new: true})
+            .then((team) => {
+                res.send(team);
+            });*/
+        console.log(req.params.nombre);
+        Team.find({teamname: req.params.nombre})
+        .then((team) => {
+            console.log(team[0].users.length);
+            res.send(team[0].users[0]);
+        })
+        .catch((err) => {
+            if(err) throw err;
+        });
+    //})
+    //.catch((err) => {
+    //      if(err) throw err;
+    //});
+};
+
+exports.team_modify_addUser_post = (req, res) => {
+    
+    Team.find({teamname: req.params.nombre})
+    .then(team => {
+
+        User.find({username: req.body.users})
+        .then(user => {
+            let addUser = team[0].users.push(user);
+            if(user) {
+                let err = "Usuario no encontrado";
+                res.render('modifyTeam.ejs', {
+                    team: team, 
+                    err: err
+                });
+            }else {
+                Team.findOneAndUpdate({teamname: req.params.nombre},
+                    {users: addUser}, {new: true})
+                .then(team => {
+                    res.render('modifyTeams.ejs', {
+                        team:team
+                    });
+                });
+            }
+
+        });
+    });
     
 };
 
+exports.team_modify_addUser_get = (req, res) => {
+    res.send("respond with a resource");
+};
+
+exports.team_modify_deleteUser_post = (req, res) => {
+    res.send("respond with a resource");
+};
+
+exports.team_modify_deleteUser_get = (req, res) => {
+    res.send("respond with a resource");
+};
--- router/teams.js
+++ router/teams.js
@@ -11,6 +11,12 @@ router.get('/createTeam', team_controllers.team_create_get);
 router.post('/createTeam', team_controllers.team_create_post);
 
 router.get('/:nombre', team_controllers.team_modify_get);
-router.post('/modifyTeam', team_controllers.team_modify_post);
+router.post('/:nombre', team_controllers.team_modify_post);
+
+router.get('/modifyuser/:nombre', team_controllers.team_modify_addUser_get);
+router.post('/modifyuser/:nombre', team_controllers.team_modify_addUser_post);
+
+//router.get('/modifyusers/:nombre',);
+//router.post('/modifyusers/:nombre',);
 
 module.exports = router;
