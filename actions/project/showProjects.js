2c12d86efd58998e81862f7a7c782ba5967cf32d
Toni <toniplsec@gmail.com>
2019-04-09 13:08:04 +0200

    metodo showProjects y sus test complemtamente implementados, refactor realizado

--- actions/project/showProjects.js
+++ actions/project/showProjects.js
@@ -2,24 +2,25 @@
 module.exports = projectRepository => {
     const teamProjects = (projectList, team) => {
         return new Promise((resolve, reject) => {
-            if(projectList === undefined || team === undefined) reject(new Error ("Lista para filtrar por equipo no definida"));
             const listTeamProjects = [];
+            if(projectList === undefined || team === undefined) reject(new Error("Lista o equipo no definidos"));
             setTimeout(() => {
-                projectList.map((x) => {
-                    if(x.teams.teamname === team.teamname) {
-                        listTeamProjects.push(x);
-                    }
+                if(projectList.length === 0) resolve(projectList);
+                projectList.map((project) => {
+                    project.teams.map((teamProject) => {
+                        if(teamProject.teamname === team.teamname) listTeamProjects.push(project);
+                    });
                 });
                 resolve(listTeamProjects);
             }, 0);
-        })
+        });
     };
 
-    return async (team) => {
+    return async team => {
         try {
             const projectList = await projectRepository.getAll();
+            if(projectList instanceof Error) throw new Error(projectList.message);
             const listTeamProjects = await teamProjects(projectList, team);
-            if(projectList.message === "Lista de proyectos no definida") throw new Error("Lista de proyectos no definida");
             return listTeamProjects;
         }catch (err){
             throw err;
--- test/unitTest/features/project/showProjects.spec.js
+++ test/unitTest/features/project/showProjects.spec.js
@@ -12,19 +12,23 @@ const mockProjectRepositoryListFailure = () => ({
     getAll: jest.fn(() => new Promise((reject) => reject(new Error("Lista de proyectos no definida"))))
 }); 
 
+const mockProjectRepositoryListFailure2 = (listProjects) => ({
+    getAll: jest.fn(() => new Promise((resolve) => resolve(listProjects)))
+});
+
 describe('Action Projects', () => {
     describe('Metodo showProjects', () => {
         it('Debe obtener la lista de proyectos, si no esta vacia', async () => {
             expect.assertions(2);
             try {
                 const user = {_id: "_id"};
-                const team1 = {teamname: "TEAMNAME"};
-                const team2 = {teamname: "TEAMNAME"};
+                const team1 = {teamname: "TEAMNAME1"};
+                const team2 = {teamname: "TEAMNAME2"};
                 const productBacklog = {_id: "_id"};
                 const kanban = {_id: "_id"};
                 const projectToCreate1 = {
                     projectname: "PROJECTNAME",
-                    teams: team1,
+                    teams: [team1],
                     responsable: user,
                     productBacklog: productBacklog,
                     kanban: kanban,
@@ -35,7 +39,7 @@ describe('Action Projects', () => {
                 };
                 const projectToCreate2 = {
                     projectname: "PROJECTNAME",
-                    teams: team2,
+                    teams: [team1, team2],
                     responsable: user,
                     productBacklog: productBacklog,
                     kanban: kanban,
@@ -54,13 +58,53 @@ describe('Action Projects', () => {
             }catch(err) {
             }
         });
+        it('Debe devolver [], si el equipo no pertenece a ningun proyecto', async () => {
+            expect.assertions();
+            try {
+                const user = {_id: "_id"};
+                const team1 = {teamname: "TEAMNAME1"};
+                const team2 = {teamname: "TEAMNAME2"};
+                const productBacklog = {_id: "_id"};
+                const kanban = {_id: "_id"};
+                const projectToCreate1 = {
+                    projectname: "PROJECTNAME",
+                    teams: [team1],
+                    responsable: user,
+                    productBacklog: productBacklog,
+                    kanban: kanban,
+                    sprints: [],
+                    client:"PROJECTCLIENT",
+                    startdate: new Date,
+                    deadline: new Date
+                };
+                const projectToCreate2 = {
+                    projectname: "PROJECTNAME",
+                    teams: [team1],
+                    responsable: user,
+                    productBacklog: productBacklog,
+                    kanban: kanban,
+                    sprints: [],
+                    client:"PROJECTCLIENT",
+                    startdate: new Date,
+                    deadline: new Date
+                };
+                const listProjects = [projectToCreate1, projectToCreate2];
+                const listTeamProjects = [];
+                const projectRepository = mockProjectRepositorygetAllSuccess(listProjects);
+                const projectAction = showProjects(projectRepository);
+                const list = await projectAction(team2);
+                expect(list).toEqual(listTeamProjects);
+            }catch(err) {
+            }
+        });
         it('Debe devolver [], si la lista esta vacia', async () => {
             expect.assertions(2);
             try {
-                const listProjects = []
+                const listProjects = [];
+                const team = {teamname: "TEAMNAME"};
                 const projectRepository = mockProjectRepositorygetAllEmpty(listProjects);
                 const projectAction = showProjects(projectRepository);
-                const list = await projectAction();
+                const list = await projectAction(team);
                 expect(list).toEqual(listProjects);
                 expect(projectRepository.getAll).toBeCalledWith();
             }catch (err) {
@@ -76,6 +120,58 @@ describe('Action Projects', () => {
                 expect(err.message).toEqual("Lista de proyectos no definida");
                 expect(err instanceof Error).toBeTruthy();
             }
-        })
+        });
+        it('Debe devolver error, si la lista para filtrar por equipo no esta definida', async () => {
+            expect.assertions(2);
+            try {
+                const listProjects = undefined;
+                const projectRepository = mockProjectRepositoryListFailure2(listProjects);
+                const projectAction = showProjects(projectRepository);
+                const list = await projectAction();
+            }catch(err) {
+                expect(err.message).toEqual("Lista o equipo no definidos");
+                expect(err instanceof Error).toBeTruthy();
+            }
+        });
+        it('Debe devolver error, si el equipo para filtrar por equipo no esta definido', async () => {
+            expect.assertions(2);
+            try {
+                const user = {_id: "_id"};
+                const team1 = {teamname: "TEAMNAME1"};
+                const team2 = undefined;
+                const productBacklog = {_id: "_id"};
+                const kanban = {_id: "_id"};
+                const projectToCreate1 = {
+                    projectname: "PROJECTNAME",
+                    teams: [team1],
+                    responsable: user,
+                    productBacklog: productBacklog,
+                    kanban: kanban,
+                    sprints: [],
+                    client:"PROJECTCLIENT",
+                    startdate: new Date,
+                    deadline: new Date
+                };
+                const projectToCreate2 = {
+                    projectname: "PROJECTNAME",
+                    teams: [team1, team2],
+                    responsable: user,
+                    productBacklog: productBacklog,
+                    kanban: kanban,
+                    sprints: [],
+                    client:"PROJECTCLIENT",
+                    startdate: new Date,
+                    deadline: new Date
+                };
+                const listProjects = [projectToCreate1, projectToCreate2];
+                const listTeamProjects = [projectToCreate2];
+                const projectRepository = mockProjectRepositorygetAllSuccess(listProjects);
+                const projectAction = showProjects(projectRepository);
+                const list = await projectAction(team2);
+            }catch(err) {
+                expect(err.message).toEqual("Lista o equipo no definidos");
+                expect(err instanceof Error).toBeTruthy();
+            }
+        });
     });
 });
