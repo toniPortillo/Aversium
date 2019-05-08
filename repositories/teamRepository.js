5377b2b041355412a09f49756b019c29a65bd68d
Toni <toniplsec@gmail.com>
2019-05-08 19:26:47 +0200

    metodo create de teamRepository implementado, faltan mas test

--- repositories/teamRepository.js
+++ repositories/teamRepository.js
@@ -1,7 +1,21 @@
 'use strict';
 module.exports = teamEntity => ({
-    create: async () => {
-
+    create: async (teamname, creator, maxmembers) => {
+        const query = {teamname: teamname};
+        const teamFound = await teamEntity.find(query);
+        try {
+            if(!teamFound.length) {
+                let saveTeam = new teamEntity({
+                    teamname: teamname,
+                    creator: creator,
+                    maxmembers: maxmembers,
+                    users: creator
+                });
+                return await saveTeam.save();
+            }
+        }catch(err) {
+            throw new Error("Equipo ya existente");
+        };
     },
     getAll: async () => {
         try {
