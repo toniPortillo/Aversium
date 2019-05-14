5f41d8ef7efd254da3440eb98ec57bc521b8d54c
Toni <toniplsec@gmail.com>
2019-05-14 12:24:51 +0200

    Implementado metodo removeTeam del action team y sus test

--- actions/team/removeTeam.js
+++ actions/team/removeTeam.js
@@ -4,21 +4,21 @@ module.exports = teamRepository => {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 if(creator === undefined) reject(new Error("Creador no definido"));
-                if(team.creator[0]._id === creator[0]._id) resolve(team);
+                else if(team[0].creator[0]._id === creator[0]._id) resolve(team);
                 reject(new Error("No puede eliminar el equipo, por no ser su responsable"));
             }, 0);
         });
-    }
+    };
 
-    return async (team, creator) => {
+    return async (team, user) => {
         if(team === undefined) throw new Error("Equipo no definido");
         try {
-            const removeTeamValidated = await validateCreator(team, creator);
+            const removeTeamValidated = await validateCreator(team, user);
             if(removeTeamValidated instanceof Error) throw new Error(removeTeamValidated.message);
             const teamDeleted = await teamRepository.removeById(removeTeamValidated[0]._id);
             if(teamDeleted instanceof Error) throw new Error(teamDeleted.message);
             const teamRemoved = {
-                team: teamRemoved,
+                team: teamDeleted,
                 message: "Equipo eliminado exitosamente"
             }
             return teamRemoved;
---
+++ test/unitTest/features/team/removeTeam.spec.js
@@ -0,0 +1,89 @@
+'use strict';
+const removeTeam = require('../../../../actions/team/removeTeam');
+
+const mockTeamRepositoryRemoveByIdSuccess = teamRemoved => ({
+    removeById: jest.fn(() => new Promise(resolve => resolve(teamRemoved)))
+});
+
+describe('Action team', () => {
+    describe('Metodo removeTeam', () => {
+        it('Debe devolver el equipo borrado, si este existe', async () => {
+            expect.assertions(2);
+            const user = [{
+                _id: "userID"
+            }];
+            const teamToDelete = [{
+                _id: "teamID",
+                teamname: "TEAMNAME",
+                creator: user,
+                maxmembers: 5,
+                users: user
+            }];
+            const teamRemoved = {
+                team: teamToDelete,
+                message: "Equipo eliminado exitosamente"
+            };
+            const teamRepository = mockTeamRepositoryRemoveByIdSuccess(teamToDelete);
+            const teamAction = removeTeam(teamRepository);
+            try {
+                const team = await teamAction(teamToDelete, user);
+                expect(team).toEqual(teamRemoved);
+                expect(teamRepository.removeById).toBeCalledWith(teamToDelete[0]._id);
+            }catch(err) {
+                throw err;
+            };
+        });
+        it('Debe devolver error, si el creador no esta definido', async () => {
+            expect.assertions(2);
+            const user = undefined;
+            const teamToDelete = [{
+                _id: "teamID",
+                teamname: "TEAMNAME",
+                creator: user,
+                maxmembers: 5,
+                users: user
+            }];
+            const teamAction = removeTeam();
+            try {
+                const team = await teamAction(teamToDelete, user);
+            }catch(err) {
+                expect(err.message).toEqual("Creador no definido");
+                expect(err instanceof Error).toBeTruthy();
+            };
+        });
+        it('Debe devolver error, si el equipo no esta definido', async () => {
+            expect.assertions(2);
+            const teamToDelete = undefined;
+            const teamAction = removeTeam();
+            try {
+                const team = await teamAction(teamToDelete);
+            }catch(err){
+                expect(err.message).toEqual("Equipo no definido");
+                expect(err instanceof Error).toBeTruthy();
+            };
+        });
+        it('Debe devolver error, si el usuario intentar eliminar el equipo sin ser su creador', async () => {
+            expect.assertions(2);
+            const user = [{
+                _id: "userID"
+            }];
+            const user1 = [{
+                _id: "userID1"
+            }];
+            const teamToDelete = [{
+                _id: "teamID",
+                teamname: "TEAMNAME",
+                creator: user1,
+                maxmembers: 5,
+                users: user1
+            }];
+            const teamAction = removeTeam();
+            try {
+                const team = await teamAction(teamToDelete, user);
+            }catch(err) {
+                expect(err.message).toEqual("No puede eliminar el equipo, por no ser su responsable");
+                expect(err instanceof Error).toBeTruthy();
+            };
+        });
+    });
+});
