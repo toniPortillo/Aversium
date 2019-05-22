a1537db25848c216e38de5d637f0cdd6af497301
Toni <toniplsec@gmail.com>
2019-05-22 13:29:25 +0200

    Implementando metodo showUser del action user, faltan mas test

---
+++ actions/user/showUser.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = userRepository => {
+    return async _id => {
+        if(_id === undefined || _id.length === 0) throw new Error("Id vacio o no definido"); 
+        try {
+            const user = await userRepository.findOneById(_id);
+            if(user instanceof Error) throw new Error(user.message);
+            return user;
+        }catch(err) {
+            throw err;
+        }
+    };  
+};
---
+++ test/unitTest/features/user/showUser.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+const showUser = require('../../../../actions/user/showUser');
+
+describe('Action user', () => {
+    describe('Metodo: showUser', () => {
+        it('Debe devolver el usuario para mostrarlo, si lo encuentra por el id', async () => {
+            expect.assertions();
+        });
+    });
+});
