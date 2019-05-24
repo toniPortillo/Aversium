ac834b5ba2e4d0e506d855b969993b4813cb6f16
Toni <toniplsec@gmail.com>
2019-05-24 17:50:50 +0200

    modifyPassword implementado, practicamente testado. Faltarian algunos para ciertos aspectos

--- actions/user/modifyPassword.js
+++ actions/user/modifyPassword.js
@@ -2,10 +2,11 @@
 module.exports = (userRepository, passwordComparer, encryptor) => {
     return async (user, oldPassword, newPassword) => {
         if(user === undefined || user.length === 0) throw new Error("Usuario vacio o no definido");
-        if(password === undefined || role.length === 0) throw new Error("Password vacia o no definida");
+        if(oldPassword === undefined || oldPassword.length === 0) throw new Error("Password actual vacia o no definida");
+        if(newPassword === undefined || newPassword.length === 0) throw new Error("Password nueva vacia o no definida");
         try {
             const validateOldPassword = await passwordComparer(oldPassword, user.password);
-            if(validateOldPassword instanceof Error) throw new Error("Error al introducir la password actual")
+            if(!validateOldPassword) throw new Error("Error al introducir la password actual")
             const newHash = await encryptor(newPassword);
             if(newHash instanceof Error) throw new Error("Error al cifrar la nueva password");
             const modifiedUser = await userRepository.modifyPassword(user._id, newHash);
--- test/unitTest/features/user/modifyPassword.spec.js
+++ test/unitTest/features/user/modifyPassword.spec.js
@@ -1,8 +1,83 @@
 'use strict';
 const modifyPassword = require('../../../../actions/user/modifyPassword');
 
+const mockUserRepositoryModifyPassword = modifiedUser => ({
+    modifyPassword: jest.fn(() => new Promise(resolve => resolve(modifiedUser)))
+});
+
+const mockPasswordComparer = () => jest.fn(() => new Promise(resolve => resolve(true)));
+
+const mockEncryptor = hash => jest.fn(() => new Promise(resolve => resolve(hash)));
+
 describe('Action user', () => {
     describe('Metodo: modifyPassword', () => {
-        it('')
+        it('Debe modificar la password del usuario, si todo esta correcto', async () => {
+            expect.assertions(4);
+            const oldPassword = "password";
+            const oldHash = "oldHash";
+            const userToModify = {
+                _id: "userId",
+                username: "username",
+                email: "userToModify@aversium.com",
+                password: oldHash,
+                role: "developer"
+            };
+            const newPassword = "noHash";
+            const hash = "hash";
+            const modifiedUser = {
+                _id: "userId",
+                username: "username",
+                email: "userToModify@aversium.com",
+                password: hash,
+                role: "developer"
+            };
+
+            const userRepository = mockUserRepositoryModifyPassword(modifiedUser);
+            const passwordComparer = mockPasswordComparer();
+            const encryptor = mockEncryptor(hash);
+            const userAction = modifyPassword(userRepository, passwordComparer, encryptor);
+            try {
+                const user = await userAction(userToModify, oldPassword, newPassword);
+                expect(user).toEqual(modifiedUser);
+                expect(userRepository.modifyPassword).toBeCalledWith(userToModify._id, hash);
+                expect(encryptor).toBeCalledWith(newPassword);
+                expect(passwordComparer).toBeCalledWith(oldPassword, userToModify.password);
+            }catch(err) {
+                throw err;
+            };
+        });
+        it('Debe devolver error, si el usuario esta vacio o no definido', async () => {
+            expect.assertions(2);
+            const userToModify = undefined;
+            const newPassword = "newPassword";
+            const oldPassword = "oldPassword";
+            const userAction = modifyPassword();
+            try {
+                const user = await userAction(userToModify, oldPassword, newPassword);
+            }catch(err) {
+                expect(err.message).toEqual("Usuario vacio o no definido");
+                expect(err instanceof Error).toBeTruthy();
+            };
+        });
+        it('Debe devolver error, si la password actual esta vacia o no definida', async () => {
+            expect.assertions(2);
+            const oldPassword = "";
+            const oldHash = "oldHash";
+            const newPassword = "newPassword"
+            const userToModify = {
+                _id: "userId",
+                username: "username",
+                email: "userToModify@aversium.com",
+                password: oldHash,
+                role: "developer"
+            };
+            const userAction = modifyPassword();
+            try {
+                const user = await userAction(userToModify, oldPassword, newPassword);
+            }catch(err) {
+                expect(err.message).toEqual("Password actual vacia o no definida");
+                expect(err instanceof Error).toBeTruthy();
+            };
+        });
     });
 });
