a8a9fe1aa5d81407bc66de5d8663f7e381fd1a30
Toni <toniplsec@gmail.com>
2019-05-27 16:16:43 +0200

    Refactor del metodo create de userRepository, para poder refactorizar el metodo userRegister del action user realizado. Ambos refactors realizados con sus respectivos test

--- test/unitTest/features/user/userRegister.spec.js
+++ test/unitTest/features/user/userRegister.spec.js
@@ -5,35 +5,22 @@ const mockUserRepositoryCreate = userToCreate => ({
     create: jest.fn(() => new Promise(resolve => resolve(userToCreate)))
 });
 
-const mockEncryptorFunction = () => jest.fn(hash => new Promise(resolve => resolve(hash)));
+const mockUserRepositoryCreateError = () => ({
+    create: jest.fn(() => new Promise(resolve => resolve(new Error("Usuario ya existente"))))
+});
+
+const mockEncryptorFunction = (hash) => jest.fn(() => new Promise(resolve => resolve(hash)));
  
 const mockEncryptorFunctionError = () => jest.fn(() => new Promise(resolve => resolve(new Error)));
 
 describe('Action user', () => {
     describe('Metodo: userRegister', () => {
-        it('Debe devolver errores de validación, si algún parámetro es incorrecto', async () => {
-            expect.assertions(2);
-            const userToCreate = {
-                username: "username",
-                email: 5,
-                password: "hash",
-                role: "developer"
-            };
-
-            const userRepository = mockUserRepositoryCreate(userToCreate);
-            const userAction = userRegister(userRepository);
-            try {
-                const user = await userAction(userToCreate);
-            }catch(err) {
-                expect(err.message).toEqual("Error: en el email de usuario");
-                expect(err instanceof Error).toBeTruthy();
-            }
-        });
         it('Debe crear un usuario, si todos los parámetros estan correctos', async () => {
             expect.assertions(3);
             const password = "password";
             const hash = "hash";
             const userToCreate = {
+                _id: "userId",
                 username: "username",
                 email: "username@aversium.com",
                 password: password,
@@ -61,6 +48,24 @@ describe('Action user', () => {
                 expect(userRepository.create).toBeCalledWith(userToCreate.username, userToCreate.email, hash, userToCreate.role);
             }catch(err) {
                 throw err;
+            }
+        });
+        it('Debe devolver errores de validación, si algún parámetro es incorrecto', async () => {
+            expect.assertions(2);
+            const userToCreate = {
+                username: "username",
+                email: 5,
+                password: "hash",
+                role: "developer"
+            };
+
+            const userRepository = mockUserRepositoryCreate(userToCreate);
+            const userAction = userRegister(userRepository);
+            try {
+                const user = await userAction(userToCreate);
+            }catch(err) {
+                expect(err.message).toEqual("Error: en el email de usuario");
+                expect(err instanceof Error).toBeTruthy();
             }
         });
         it('Debe devolver error de validación, si la validación del username es incorrecta', async () => {
@@ -153,6 +158,35 @@ describe('Action user', () => {
                 expect(err instanceof Error).toBeTruthy();
             }
         });
+        it('Debe devolver error, si el usuario ya existe', async () => {
+            expect.assertions(4);
+            const password = "password";
+            const userToCreate = {
+                username: "username",
+                email:"usertocreate@aversium.com",
+                password: password,
+                role: "scrumMaster"
+            };
+            const hash = "hash";
+            const encryptedUser = {
+                username: "username",
+                email: "usertocreate@aversium.com",
+                password: "hash",
+                role: "scrumMaster"
+            };
+            const userRepository = mockUserRepositoryCreateError();
+            const encryptor = mockEncryptorFunction(hash);
+            const userAction = userRegister(userRepository, encryptor);
+            try {
+                const user = await userAction(userToCreate);
+            }catch(err) {
+                expect(err.message).toEqual("Usuario ya existente");
+                expect(err instanceof Error).toBeTruthy();
+                expect(encryptor).toBeCalledWith(userToCreate.password);
+                expect(userRepository.create).toBeCalledWith(userToCreate.username, userToCreate.email,
+                    hash, userToCreate.role);
+            };
+        });
         it('Debe devolver error, si el usuario no esta definido', async () => {
             expect.assertions(2);
             const userToCreate = undefined;
@@ -177,7 +211,7 @@ describe('Action user', () => {
             }catch(err) {
                 expect(err.message).toEqual("Usuario vacio o no definido");
                 expect(err instanceof Error).toBeTruthy();
-            }
+            };
         });
     });
 });
