b2bec537fbd522baeb1efe42844444902cec3bf1
Toni <toniplsec@gmail.com>
2019-05-31 13:36:56 +0200

    Refactorizando metodo modifyMembersNumber del action team, poq que no se valida el numero maximo de miembros

--- actions/team/modifyMembersNumber.js
+++ actions/team/modifyMembersNumber.js
@@ -4,8 +4,18 @@ module.exports = teamRepository => {
         return new Promise((resolve, rejects) => {
             setTimeout(() => {
                 if(creator === undefined) reject(new Error("Creador no definido"));
-                else if(team[0].creator[0]._id === creator._id) resolve(team);
+                else if(team[0].creator[0]._id === creator[0]._id) resolve(team);
                 else reject(new Error("No puede modificar el equipo, por no ser su responsable"));
+            }, 0);
+        });
+    };
+
+    const _validateMaxMembers = (team, maxMembers) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(team[0].users.length > maxMembers) reject(new Error("Existe ya un número mayor de miembros, de los que quiere restringir"));
+                else if(team[0].users.length === maxMembers) reject(new Error("El número de miembros es igual, a los que quiere restringir"));
+                else resolve(team);
             }, 0);
         });
     };
@@ -21,7 +31,7 @@ module.exports = teamRepository => {
             if(modifiedTeam instanceof Error) throw new Error(modifiedTeam.message);
             const resultingTeam = {
                 team: modifiedTeam,
-                message: "Equipo modificado exitosamente"
+                message: "Numero máximo de miembros cambiado"
             };
             return resultingTeam;
         }catch(err) {
--- test/unitTest/features/team/modifyMembersNumber.spec.js
+++ test/unitTest/features/team/modifyMembersNumber.spec.js
@@ -6,6 +6,20 @@ describe('Action team', () => {
     describe('Metodo: modifyMembersNumber', () => {
         it('Debe devolver el equipo modificado, si este existe', async () => {
             expect.assertions(2);
+            const creator = [{
+                _id: "creatorId"
+            }];
+            const teamToModify = [{
+                _id: "teamId",
+                teamname: "teamtToModify",
+                creator: creator,
+                maxmembers:3
+            }];
+            try {
+
+            }catch(err) {
+                throw err;
+            }
         });
     });
 });
