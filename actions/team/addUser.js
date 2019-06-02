7377dd2724e147dfd8ef66336bd18922b7a1e420
Toni <toniplsec@gmail.com>
2019-06-02 21:28:04 +0200

    Implementando metodo addUser del action Team

--- actions/team/addUser.js
+++ actions/team/addUser.js
@@ -1,21 +1,42 @@
 'use strict';
 module.exports = (teamRepository, checkTeamService) => {
-    const validateCreator = (team, creator) => {
+    const _validateCreator = (team, creator) => {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-                if(creator === undefined) reject(new Error("Creador no definido"));
+                if(creator === undefined || creator.length === 0) reject(new Error("Creador vacio o no definido"));
                 else if(team[0].creator[0]._id === creator[0]._id) resolve(team);
                 reject(new Error("No puede modificar el equipo, por no ser su responsable"));
             }, 0);
         });
     };
 
+    const _validateTeamCapacity = team => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(team[0].users.length < team[0].maxmembers) resolve(team);
+                else reject(new Error("No se pudo añadir este usuario, por que supera la capacidad del equipo"));
+            }, 0);
+        });
+    };
+
     return async (team, user, newMember) => {
-        if(team === undefined) throw new Error("Equipo no definido");
+        if(team === undefined || team.length === 0) throw new Error("Equipo vacio o no definido");
         try {
-            const modifyTeamValidated = await validateCreator(team, user);
-            if(modifyTeamValidated instanceof Error) throw new Error(modifyTeamValidated.message);
-            const checkTeam = checkTeamService(team[0], user [0])
+            const validateCreator = await _validateCreator(team, user);
+            if(validateCreator instanceof Error) throw new Error(validateCreator.message);
+            const validateTeamCapacity = await _validateTeamCapacity(team);
+            if(validateTeamCapacity instanceof Error) throw new Error(validateTeamCapacity.message);
+            const checkTeam = checkTeamService(team[0], newMember[0]);
+            const validatedMember = await checkTeam();
+            if(validatedMember instanceof Error) throw new Error(validatedMember.message);
+            validateTeamCapacity[0].users.push(validatedMember);
+            const modifiedTeam = await teamRepository.modifyUsers(validateTeamCapacity[0]._id, validateTeamCapacity[0].users);
+            if(modifiedTeam instanceof Error) throw new Error(modifiedTeam.message);
+            const resultingTeam = {
+                team: modifiedTeam,
+                message: "Usuario añadido con exito"
+            };
+            return resultingTeam;
         }catch(err) {
             throw err;
         };
