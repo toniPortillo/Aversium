9908bb6ba9ce1bf557afd422382d47b7470c3330
Toni <toniplsec@gmail.com>
2019-06-04 19:24:01 +0200

    Testando metodo addUser del action Team

--- test/unitTest/features/team/addUser.spec.js
+++ test/unitTest/features/team/addUser.spec.js
@@ -1,14 +1,65 @@
 'use strict';
 const addUser = require('../../../../actions/team/addUser');
 
+const mockUserRepository = user => ({
+    findOneByEmail: jest.fn(() => new Promise(resolve => resolve(user)))
+});
+
+const mockCheckTeamService = user => jest.fn(() => new Promise(resolve => resolve(user)));
+
+const mockTeamRepository = team => ({
+    modifyUsers: jest.fn (() => new Promise(resolve => resolve(team)))
+});
+
 describe('Action team', () => {
     describe('Metodo: addUser', () => {
         it('Debe devolver el equipo modificado con el nuevo usuario, si no ocurre ningun problema', async () => {
             expect.assertions(2);
             const creator = [{
-                _id: "creatorID"
+                _id: "creatorID",
+                username: "creator",
+                email: "creator@aversium.es",
+                role: "productOwner"
             }];
-            const 
+            const user = [{
+                _id: "user3",
+                username: "user",
+                email: "user@aversium.com",
+                role: "developer",
+            }];
+            const users = [
+                {
+                    _id: "user1",
+                    role: "scrumMaster",
+                },
+                {
+                    _id: "user2",
+                    role: "developer",
+                },
+                {
+                    _id: "user4",
+                    role: "developer",
+                },
+                {
+                    _id: "creatorID",
+                    username: "creator",
+                    email: "creator@aversium.es",
+                    role: "productOwner"
+                }
+            ];
+            const team = [{
+                id: "teamId",
+                teamname: "teamname",
+                creator: creator,
+                maxmembers: 5,
+                users: users
+            }];
+                
+            try {
+
+            }catch(err) {
+                throw err;
+            };
         });
     });
 });
