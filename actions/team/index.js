63db3b538a5ffe2e016d73eff484e348ffaa42ec
Toni <toniplsec@gmail.com>
2019-06-07 13:12:09 +0200

    Reagrupando metodos del action team, en su respectivo index

--- actions/team/index.js
+++ actions/team/index.js
@@ -1,2 +1,16 @@
 'use strict';
 const repositories = require('../../repositories/index');
+
+const userRepository = repositories.User;
+const teamRepository = repositories.Team;
+
+const createCreateTeam = require('./createTeam');
+const createShowTeams = require('./showTeams');
+const createShowTeam = require('./showTeam');
+const createRemoveTeam = require('./removeTeam');
+const createModifyMembersNumber = require('./modifyMembersNumber');
+const createAddUser = require('./addUser');
+
+module.exports = {
+    
+};
