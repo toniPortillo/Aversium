3dd5e7f697214c8b44be95868493f4e6d440df77
Toni <toniplsec@gmail.com>
2019-06-10 22:33:13 +0200

    Implementando index del action project

---
+++ actions/project/index.js
@@ -0,0 +1,6 @@
+'use strict';
+const repositories = require('../../repositories/index');
+
+const userRepository = repositories.User;
+const teamRepository = repositories.Team;
+const projectRepository = repositories.Project;
