64ada39db7f23d0fd37b9826b811ed9c6f5e7146
Toni <toniplsec@gmail.com>
2019-06-11 18:47:16 +0200

    Seguimos implementando el index del action project

--- actions/project/index.js
+++ actions/project/index.js
@@ -4,3 +4,9 @@ const repositories = require('../../repositories/index');
 const userRepository = repositories.User;
 const teamRepository = repositories.Team;
 const projectRepository = repositories.Project;
+
+const createCreateProject = require('./createProject');
+const createShowProjects = require('./showProjects');
+const createShowProject = require('./showProject');
+const createModifyProject = require('./modifyProject');
+const createRemoveProject = require('./removeProject');
